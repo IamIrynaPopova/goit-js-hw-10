fd2ef1bae03c3357b1a6303cde3f1847188929eb
Iryna Popova <popovaira969@gmail.com>
2023-01-30 01:12:38 +0200

    add

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+const ENDPOINT = 'https://restcountries.com/v3.1/name/'
+
+
+ function fetchCountries(query) {
+   return fetch(`${ENDPOINT}${query}`).then(res => res.json());
+ }
+
+
+
+
+
+ 
+
+export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputRef = document.getElementById('search-box');
+const ulEL = document.querySelector('.country-list');
+const divEL = document.querySelector('.country-info');
+
+inputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch() {
+clearHistory();
+const valueSearch = inputRef.value.trim();
+return fetchCountries(valueSearch).then(data => createMarkup(data));
+};
+
+function clearHistory() {
+divEL.innerHTML='';      
+}
+
+
+function createMarkup(value) {
+  const markup = value.map(
+    el => `<img src = ${el.flags.svg} width ='50'></img>
+                <h1 class="country-name" >${el.name.official}</h1>
+                <p class="country-capital" >Capital: ${el.capital}</p>
+                <p class="country-population" >Population: ${
+                  el.population
+                }</p>
+                <p class="country-languages">Languages: ${Object.values(
+                  el.languages
+                )}</p> `
+  );
+
+  return divEL.insertAdjacentHTML('afterbegin', markup);
+}
